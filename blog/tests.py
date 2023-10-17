7a5f008e88193f7b7b10b3bcbfa94332ce5ea519
Suraj Mhatre <surajmcodes@gmail.com>
2023-10-17 13:03:34 +0530

    CURD operations added

--- blog/tests.py
+++ blog/tests.py
@@ -23,6 +23,9 @@ class BlogTest(TestCase):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
 
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/' )
+
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
         self.assertEqual(f'{self.post.body}', 'A nice body content')
@@ -44,3 +47,23 @@ class BlogTest(TestCase):
         self.assertTemplateUsed(response, 'post_detail.html')
 
 
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+                'title': 'New Title',
+                'body': 'New Text',
+                'author': self.user
+            })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'New Title')
+        self.assertContains(response, 'New Text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+                'title': 'Updated Title',
+                'body': 'Updated Text',
+            })
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.get(reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 200)
